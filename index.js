9795828eaa05a33de27107c696c2c12bcb0c0ff1
MicaelFN <micael.f.noronha@gmail.com>
2020-05-05 17:26:44 -0300

    interpolateJSX

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import ValidateWithPrototype from './src/validateWithProptype';
-import SideEffects from './src/sideEffects'
+import SideEffects from './src/sideEffects';
+import interpolateJSX from './src/interpolateJSX'
 
-AppRegistry.registerComponent(appName, () => SideEffects);
+AppRegistry.registerComponent(appName, () => interpolateJSX);
--- src/interpolateJSX.js
+++ src/interpolateJSX.js
@@ -4,7 +4,7 @@ import {View, Text, StyleSheet} from 'react-native';
 // Reescreva a componente abaixo alterando XXXX pelo nome e X pelo número de letras no nome.
 const Texto = (props) => (
   <View style={style.row}>
-    <Text>O nome completo é XXXXXXX e tem X letras</Text>
+    <Text>O nome completo é {props.firstName} {props.lastName} e tem {props.firstName.length + props.lastName.length} letras</Text>
   </View>
 );
 
